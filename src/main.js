f94f7ec5f7dffdd87cb0dfdac254209f803ab45b
HAIZAKURA <hhbilly99@gmail.com>
2020-04-25 21:16:37 +0800

    use element-theme-ink

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+// import 'element-ui/lib/theme-chalk/index.css'
+import 'element-theme-ink'
 import router from './router'
 import store from './store'
 

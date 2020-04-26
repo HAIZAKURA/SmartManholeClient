79f3bfbae6b2281c9c31ee3347266ee7c725267b
HAIZAKURA <hhbilly99@gmail.com>
2020-04-26 21:33:40 +0800

    add axios

--- src/main.js
+++ src/main.js
@@ -4,10 +4,11 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
 Vue.config.productionTip = false
-
 Vue.use(ElementUI)
+Vue.prototype.$axios = axios
 
 new Vue({
   router,

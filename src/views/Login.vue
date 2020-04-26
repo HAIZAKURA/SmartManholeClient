4b942b71aa58ebafdf99222bde011efcc773adea
HAIZAKURA <hhbilly99@gmail.com>
2020-04-26 21:34:15 +0800

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -9,13 +9,13 @@
           <div class="item">
             <el-form label-position="left" label-width="60px" :model="loginForm">
               <el-form-item label="用户名">
-                <el-input type="text" autofocus clearable v-model="loginForm.uname"></el-input>
+                <el-input type="text" clearable v-model="loginForm.uname"></el-input>
               </el-form-item>
               <el-form-item label="密码">
                 <el-input type="password" show-password clearable v-model="loginForm.upass"></el-input>
               </el-form-item>
             </el-form>
-            <el-button type="primary" @click="ulogin">登&nbsp;&nbsp;录</el-button>
+            <el-button type="primary" @click="loginFunc">登&nbsp;&nbsp;录</el-button>
           </div>
         </el-card>
       </div>
@@ -24,6 +24,8 @@
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
+
 export default {
   name: 'Login',
   data() {
@@ -34,8 +36,49 @@ export default {
       }
     }
   },
+  computed: {
+    ...mapGetters(['uname', 'uauth', 'udept'])
+  },
+  created: function() {
+    // if (this.uname != 'null' && this.uauth != 'null' && this.udept != null) {
+    //   this.$router.push({ path: '/' })=
+    // }
+    this.$axios.get('/api/check')
+      .then(res => {
+        if (res.data.status == 'success') {
+          this.setUname(res.data.uname)
+          this.setUauth(res.data.uauth)
+          this.setUdept(res.data.udept)
+          this.$router.push({ path: '/' })
+        }
+      })
+  },
   methods: {
-    ulogin() {}
+    loginFunc() {
+      // this.setUname(this.loginForm.uname)
+      // this.$router.push({ path: '/' })
+      this.$axios.post('/api/user/login', this.loginForm)
+        .then(res => {
+          if (res.data.status == 'success') {
+            this.setUname(res.data.uname)
+            this.setUauth(res.data.uauth)
+            this.setUdept(res.data.udept)
+            this.$message({
+              message: '登录成功',
+              type: 'success'
+            })
+            setTimeout(() => {
+              this.$router.push({ path: '/' })
+            }, 1000)
+          } else {
+            this.$message({
+              message: '用户名或密码错误',
+              type: 'error'
+            })
+          }
+        })
+    },
+    ...mapActions(['setUname', 'setUauth', 'setUdept'])
   }
 }
 </script>

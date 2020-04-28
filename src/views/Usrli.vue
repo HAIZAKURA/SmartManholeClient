4632ab5f535819bc7b0e02321d14f584e0cb03f5
HAIZAKURA <hhbilly99@gmail.com>
2020-04-28 21:37:15 +0800

    create user list page

---
+++ src/views/Usrli.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="usrli">
+    <p>User List</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Usrli'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>

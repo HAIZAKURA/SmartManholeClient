f13e5789a290a37a3e0ad74a8c3f7f84c9c9818a
HAIZAKURA <hhbilly99@gmail.com>
2020-04-28 21:36:43 +0800

    create deive map page

---
+++ src/views/Devma.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="devma">
+    <p>Device Map</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Devma'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>

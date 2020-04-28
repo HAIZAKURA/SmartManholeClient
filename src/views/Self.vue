b6316a2e69eb5228e10358bd2c65d9d13086ea9e
HAIZAKURA <hhbilly99@gmail.com>
2020-04-28 21:37:08 +0800

    create self info page

---
+++ src/views/Self.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="self">
+    <p>Self Info</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Self'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>

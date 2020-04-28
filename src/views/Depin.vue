816c8161893470589e95454c49e4784fab5e7585
HAIZAKURA <hhbilly99@gmail.com>
2020-04-28 21:35:29 +0800

    create dept info page

---
+++ src/views/Depin.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="depin">
+    <p>Dept Info</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Depin'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>

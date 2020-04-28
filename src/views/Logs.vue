958fc84846f6d500633abf1b6e6f2d83f36315b1
HAIZAKURA <hhbilly99@gmail.com>
2020-04-28 21:36:59 +0800

    create logs page

---
+++ src/views/Logs.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="logs">
+    <h1>This is an about2 page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Logs'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>

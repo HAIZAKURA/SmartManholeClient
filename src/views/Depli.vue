acbdaefccd0828075e1572af040d20d0d9087700
HAIZAKURA <hhbilly99@gmail.com>
2020-04-28 21:36:11 +0800

    create dept list page

---
+++ src/views/Depli.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="devli">
+    <p>Dept List</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Depli'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>

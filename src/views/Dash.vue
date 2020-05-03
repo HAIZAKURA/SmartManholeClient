58cbc1767d521a2ead7b6e2061427fec5e4a2688
HAIZAKURA <hhbilly99@gmail.com>
2020-05-03 19:55:26 +0800

    Update Dash.vue

--- src/views/Dash.vue
+++ src/views/Dash.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="dash">
-    <p>Dash</p>
+    <div class="title">
+      <span class="title-text">监&nbsp;控</span>
+    </div>
+    <el-divider></el-divider>
   </div>
 </template>
 
@@ -11,5 +14,12 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-
+.title
+  text-align left
+  &-text
+    font-size 2em
+  &-btn
+    height 2em
+    line-height 2em
+    float right
 </style>
